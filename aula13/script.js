c8708860f9e9d9478cc9edc328f34f73910d26a5
Rodrigo Oliveira <144641235+Rjodrigo@users.noreply.github.com>
2023-11-29 09:43:55 -0300

    Exercicios JavaScript Parte2
    Relogio customizado com JavaScript

---
+++ aula13/script.js
@@ -0,0 +1,21 @@
+function carregar() {
+    var msg =window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    //var hora = 
+    msg.innerHTML = `Agora são ${hora} horas.` // ${}= A Placeholder
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA
+        img.src = 'manha.png'
+        document.body.style.background = '#fee772'
+    } else if (hora >= 12 && hora <= 18) {
+        //BOA TARDE
+        img.src = "tarde.png"
+        document.body.style.background = '#ab674a'
+    } else { 
+        //BOM NOITE
+        img.src = 'noite.png'
+        document.body.style.backeground = '#1c2f49'
+    }
+}
